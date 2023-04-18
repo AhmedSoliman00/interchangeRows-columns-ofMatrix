a2480f9a9c46fd7382e76db53d24b8274ca46dc4
hp <himozhima@gmail.com>
2023-04-18 21:04:05 +0200

    Implementaion of interchanging rows (fourth commit)

--- MatrixInterchange.java
+++ MatrixInterchange.java
@@ -22,5 +22,32 @@ public class MatrixInterchange {
             }
             System.out.println();
         }
+        int choice = 0;
+        while (true) {
+            System.out.println("Enter 1 to interchange rows");
+            System.out.println("Enter 2 to interchange columns");
+            System.out.println("Enter 3 to Exit");
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the row numbers:");
+                    int row1 = scan.nextInt(); // to Select rows you want to interchange
+                    int row2 = scan.nextInt();
+                    int temp = 0;
+                    for (int i = 0; i < nOfRows; i++) {
+                        temp = arr[row1 - 1][i];           // to interchange positions between rows
+                        arr[row1 - 1][i] = arr[row2 - 1][i];
+                        arr[row2 - 1][i] = temp;
+                    }
+                    System.out.println("Matrix after interchanging row " + row1 + "with" + row2);
+                    for (int i = 0; i < nOfRows; i++) {
+                        for (int j = 0; j < nOfColumns; j++) {
+                            System.out.print(arr[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+
+            }
+        }
     }
 }
---
+++ out/production/InterchangeRows(columns)ofMatrix/README.md
@@ -0,0 +1,1 @@
+# interchangeRows-columns-ofMatrix
