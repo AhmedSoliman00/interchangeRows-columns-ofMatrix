48bce942d3035c9d066847e7334e91a324c8ab24
hp <himozhima@gmail.com>
2023-04-18 16:00:22 +0200

    second commimt (taking inputs to make the matrix)

--- MatrixInterchange.java
+++ MatrixInterchange.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
+
 public class MatrixInterchange {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int nOfRows, nOfColumns;
+        System.out.println("Enter Number Of Rows:");
+        nOfRows = scan.nextInt();
+        System.out.println("Enter Number Of Columns:");
+        nOfColumns = scan.nextInt();
+        int arr[][] = new int[nOfRows][nOfColumns];
+        System.out.println("Enter Matrix Element:");
+        for (int i = 0; i < nOfRows; i++) {
+            for (int j = 0; j < nOfColumns; j++) {
+                arr[i][j] = scan.nextInt();
+            }
+        }
         
     }
 }
